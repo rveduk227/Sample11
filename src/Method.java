70a238b5a9788d7131893b9d707c766f270e4ccb
Rajeswari V <Rajeswari V@DESKTOP-RRBPV60>
2021-03-03 16:52:33 +0530

    Commit...

---
+++ src/Method.java
@@ -0,0 +1,18 @@
+
+public class Method {
+
+	
+	 int add(int a,int b) {
+		 int c=a+b;
+		 return c;
+		 
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Method m=new Method();
+		System.out.println(m.add(45, 60));
+
+	}
+
+}
---
+++ src/Strng.java
@@ -0,0 +1,19 @@
+
+public class Strng {
+
+	double add(double d1, double d2) {
+		double d3=d1+d2;
+		return d3;
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
